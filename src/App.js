f6d7534496bedd6f7ef90d495d10a7871d305b7f
vansh953 <vansh24164035@akgec.ac.in>
2025-10-25 16:48:58 +0530

    Fixed AuthCallBack import, jwtDecode issue, and completed App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ import MyProfile from "./pages/MyProfile";
 import Bookmarks from "./pages/Bookmarks";
 import WatchHistory from "./pages/WatchHistory";
 import Home from "./pages/Home";
-import AuthCallBack from "./pages/AuthCallBack";
+import AuthCallBack from "./pages/AuthCallBack.js";
+
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
